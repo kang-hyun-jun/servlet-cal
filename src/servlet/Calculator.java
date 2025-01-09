a63f859dc921ec7ea8bda74cd06c8b4e9be1ff42
HyeonJun Kang <kjun2003@naver.com>
2025-01-09 15:29:29 +0900

    cookie

--- src/servlet/Calculator.java
+++ src/servlet/Calculator.java
@@ -12,11 +12,11 @@ public class Calculator extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         ServletContext application = request.getServletContext(); // application객체 (servlet context)
-        HttpSession session = request.getSession();
+        //HttpSession session = request.getSession();
         //사용자 입력으로부터 value값과 cal값 획득
         String value = request.getParameter("value");
         String cal = request.getParameter("cal");
-
+        Cookie[] cookies = request.getCookies();
         //value에 초기값 부여 및 정수화
         int value_int = 0;
         if (value != null && !value.equals("")) {
@@ -29,10 +29,27 @@ public class Calculator extends HttpServlet {
         //값을 계산
         if (cal.equals("=")) {
             //int x = (Integer) application.getAttribute("value");
-            int x = (Integer) session.getAttribute("value");
+            //int x = (Integer) session.getAttribute("value");
+            int x =0;
+            String cal_str = "";
+            for(int i=0;i<cookies.length;i++)
+            {
+                Cookie c = cookies[i];
+                if (c.getName().equals("value")) {
+                    x = Integer.parseInt(c.getValue());
+                    break;
+                }
+            }
+            for(int i=0;i<cookies.length;i++) {
+                Cookie c = cookies[i];
+                if (c.getName().equals("cal")) {
+                    cal_str =c.getValue();
+                    break;
+                }
+            }
             int y = value_int;
             //String cal_str = (String) application.getAttribute("cal");
-            String cal_str = (String) session.getAttribute("cal");
+            //String cal_str = (String) session.getAttribute("cal");
             if (cal_str.equals("+")) {
                 int sum = x + y;
                 out.println("<h1>계산 결과 : "+sum+"</h1>");
@@ -54,8 +71,16 @@ public class Calculator extends HttpServlet {
         {
             //application.setAttribute("value", value_int);//요소 저장
             //application.setAttribute("cal", cal);//요소 저장
-            session.setAttribute("value", value_int);//요소 저장
-            session.setAttribute("cal", cal);//요소 저장
+
+            //session.setAttribute("value", value_int);//요소 저장
+            //session.setAttribute("cal", cal);//요소 저장
+            Cookie valCookie = new Cookie("value", String.valueOf(value_int));
+            Cookie calcookie = new Cookie("cal", cal);
+            valCookie.setPath("/Calculator");
+            valCookie.setMaxAge(60*60*24);
+            calcookie.setPath("/Calculator");
+            response.addCookie(valCookie);
+            response.addCookie(calcookie);
         }
 
 
