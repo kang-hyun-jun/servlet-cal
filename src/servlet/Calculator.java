31a098cd2bde74f7dd6d3c69809241b70e8488cf
HyeonJun Kang <kjun2003@naver.com>
2025-01-09 15:31:30 +0900

    Redirect

--- src/servlet/Calculator.java
+++ src/servlet/Calculator.java
@@ -81,6 +81,7 @@ public class Calculator extends HttpServlet {
             calcookie.setPath("/Calculator");
             response.addCookie(valCookie);
             response.addCookie(calcookie);
+            response.sendRedirect("Calculator.html");
         }
 
 
