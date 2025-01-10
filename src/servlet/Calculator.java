6b5ccfcfc0f1e01d6f65f9b5091051b86c1ebf11
HyeonJun Kang <kjun2003@naver.com>
2025-01-10 19:33:00 +0900

    Dynamic Web Page

--- src/servlet/Calculator.java
+++ src/servlet/Calculator.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -11,84 +14,53 @@ public class Calculator extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        ServletContext application = request.getServletContext(); // application객체 (servlet context)
-        //HttpSession session = request.getSession();
-        //사용자 입력으로부터 value값과 cal값 획득
+        Cookie[] cookies = request.getCookies();
+
         String value = request.getParameter("value");
         String cal = request.getParameter("cal");
-        Cookie[] cookies = request.getCookies();
-        //value에 초기값 부여 및 정수화
-        int value_int = 0;
-        if (value != null && !value.equals("")) {
-            value_int = Integer.parseInt(value);
-        }else
-        {
-            out.println("<h1>올바른 숫자를 입력하세요!</h1>");
+        String dot = request.getParameter("dot");
+
+        String exp = "";
+
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie c = cookies[i];
+                if (c.getName().equals("exp")) {
+                    exp = c.getValue();
+                    break;
+                }
+            }
         }
 
-        //값을 계산
-        if (cal.equals("=")) {
-            //int x = (Integer) application.getAttribute("value");
-            //int x = (Integer) session.getAttribute("value");
-            int x =0;
-            String cal_str = "";
-            for(int i=0;i<cookies.length;i++)
-            {
-                Cookie c = cookies[i];
-                if (c.getName().equals("value")) {
-                    x = Integer.parseInt(c.getValue());
-                    break;
-                }
-            }
-            for(int i=0;i<cookies.length;i++) {
-                Cookie c = cookies[i];
-                if (c.getName().equals("cal")) {
-                    cal_str =c.getValue();
-                    break;
-                }
-            }
-            int y = value_int;
-            //String cal_str = (String) application.getAttribute("cal");
-            //String cal_str = (String) session.getAttribute("cal");
-            if (cal_str.equals("+")) {
-                int sum = x + y;
-                out.println("<h1>계산 결과 : "+sum+"</h1>");
-            } else if (cal_str.equals("*")) {
-                int sum = x * y;
-                out.println("<h1>계산 결과 : "+sum+"</h1>");
-            } else if (cal_str.equals("-"))
-            {
-                int sum = x - y;
-                out.println("<h1>계산 결과 : "+sum+"</h1>");
-            } else if (cal_str.equals("/"))
-            {
-                int sum = x / y;
-                out.println("<h1>계산 결과 : "+sum+"</h1>");
+        if (cal != null && cal.equals("="))
+        {
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            try {
+                exp = String.valueOf(engine.eval(exp));
+            } catch (ScriptException e) {
+                throw new RuntimeException(e);
             }
         }
-        //값을 저장
+        else if (cal != null && cal.equals("C"))
+        {
+            exp ="";
+        }
         else
         {
-            //application.setAttribute("value", value_int);//요소 저장
-            //application.setAttribute("cal", cal);//요소 저장
-
-            //session.setAttribute("value", value_int);//요소 저장
-            //session.setAttribute("cal", cal);//요소 저장
-            Cookie valCookie = new Cookie("value", String.valueOf(value_int));
-            Cookie calcookie = new Cookie("cal", cal);
-            valCookie.setPath("/Calculator");
-            valCookie.setMaxAge(60*60*24);
-            calcookie.setPath("/Calculator");
-            response.addCookie(valCookie);
-            response.addCookie(calcookie);
-            response.sendRedirect("Calculator.html");
+            exp += (value==null) ? "" : value;
+            exp += (cal==null) ? "" : cal;
+            exp += (dot==null) ? "" : dot;
         }
 
 
 
-        out.println("<a href=\"Calculator.html\">다시 계산하기!</a><br>");
+        Cookie expCookie = new Cookie("exp", exp);
+        if (cal != null && cal.equals("C"))
+        {
+            expCookie.setMaxAge(0);
+        }
+        response.addCookie(expCookie);
 
-
-
+        response.sendRedirect("Calculator2");
     }
 }
