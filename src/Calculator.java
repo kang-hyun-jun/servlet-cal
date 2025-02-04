62921ef01e2a44c68f63a78d26355dfd04953a74
Hyeonjun Kang <kjun2003@naver.com>
2025-02-05 00:52:42 +0900

    calc implement by me

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,6 +10,7 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Stack;
 
 @WebServlet("/Calculator")
 public class Calculator extends HttpServlet {
@@ -36,12 +37,7 @@ public class Calculator extends HttpServlet {
 
         if (cal != null && cal.equals("="))
         {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-            try {
-                exp = String.valueOf(engine.eval(exp));
-            } catch (ScriptException e) {
-                throw new RuntimeException(e);
-            }
+            exp = calculator(exp);
         }
         else if (cal != null && cal.equals("C"))
         {
@@ -64,5 +60,91 @@ public class Calculator extends HttpServlet {
         response.addCookie(expCookie);
 
         response.sendRedirect("myServlet");
+    }
+    public String calculator(String exp)
+    {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<Character> calStack = new Stack<>();
+        String num_temp="";
+        for(int i = 0; i < exp.length(); i++)
+        {
+            char c = exp.charAt(i);
+            if(classification(c))
+            {
+                //분류상으로 연산자인 경우
+                numStack.push(Integer.parseInt(num_temp));
+                num_temp="";
+                if(!calStack.isEmpty()&&weight(c)<=calStack.peek())
+                {
+                    int a = numStack.pop();
+                    int b = numStack.pop();
+                    char op = calStack.pop();
+                    numStack.push(applyOperation(a, b, op));
+                }
+                else
+                {
+                    calStack.push(c);
+                }
+            }
+            else
+            {
+                //숫자인경우
+                num_temp+=String.valueOf(exp.charAt(i));
+            }
+        }
+        numStack.push(Integer.parseInt(num_temp));
+
+        while (!calStack.isEmpty()) {
+            int b = numStack.pop();
+            int a = numStack.pop();
+            char op = calStack.pop();
+            numStack.push(applyOperation(a, b, op));
+        }
+        exp = numStack.pop().toString();
+        System.out.println(exp);
+        return exp;
+    }
+    public int weight(char cal)
+    {
+        if((cal=='+')||(cal=='-'))
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+    public boolean classification(char cal)
+    {
+        if((cal=='+')||(cal=='-')||(cal=='*')||(cal=='/'))
+        {
+            return true;
+            //연산자
+        }
+        else
+        {
+            return false;
+            //숫자
+        }
+    }
+    public int applyOperation(int a, int b, char op)
+    {
+        if(op=='+')
+        {
+            return a+b;
+        }
+        else if(op=='-')
+        {
+            return a-b;
+        }
+        else if(op=='*')
+        {
+            return a*b;
+        }
+        else {
+            return a/b;
+        }
+
     }
 }
